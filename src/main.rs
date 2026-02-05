087e25db271e4853b29471124a9feb1f56b1d029
Max Liu <max_liu@ucsb.edu>
2026-02-05 15:40:58 -0800

    asm parsing, finish syscalls, add stack memory

--- src/main.rs
+++ src/main.rs
@@ -1,76 +1,10 @@
+use std::{env, fs};
+
 fn main() {
-    let mut machine = Interpreter::new();
-    // machine.load(vec![
-    //     0x20020010,
-    //     0x0000000c
-    // ]);
-    machine.load(vec![
-        0x240400c0,
-        0x00042400,
-        0x24840000,
-        0x20020004,
-        0x0000000c,
-        0x20020005,
-        0x0000000c,
-        0x00026020,
-        0x218c0001,
-        0x200a0001,
-        0x000a2020,
-        0x000a1020,
-        0x0c100027,
-        0x00025820,
-        0x240400c0,
-        0x00042400,
-        0x248400d4,
-        0x20020004,
-        0x0000000c,
-        0x000a2020,
-        0x20020001,
-        0x0000000c,
-        0x240400c0,
-        0x00042400,
-        0x248400d8,
-        0x20020004,
-        0x0000000c,
-        0x000b2020,
-        0x20020001,
-        0x0000000c,
-        0x240400c0,
-        0x00042400,
-        0x248400dc,
-        0x20020004,
-        0x0000000c,
-        0x214a0001,
-        0x154cffe5,
-        0x2002000a,
-        0x0000000c,
-        0x10800015,
-        0x1000ffd7,
-        0x00000022,
-        0xafbf0000,
-        0x00000022,
-        0x0c100027,
-        0x00000020,
-        0x8fbf0000,
-        0x00000020,
-        0x00000022,
-        0xafa20000,
-        0x00000022,
-        0xafbf0000,
-        0x00000022,
-        0x0c100027,
-        0x00000020,
-        0x8fbf0000,
-        0x00000020,
-        0x8fb70000,
-        0x00000020,
-        0x00571020,
-        0x03e00008,
-        0x20020000,
-        0x03e00008,
-        0x20020001,
-        0x03e00008,
-    ]);
+    let file = fs::read_to_string(env::args().nth(1).expect("Program file required"))
+        .expect("Program file unreadable");
+    let mut machine =
+        Interpreter::new(Program::parse(file.as_str()).expect("Program file corrupted"));
     let exitcode = loop {
         if let Err(e) = machine.step() {
             break e;
@@ -80,121 +14,201 @@ fn main() {
     std::process::exit(exitcode)
 }
 
+struct Program {
+    text: Vec<u32>,
+    data: Vec<u32>,
+    stack: Vec<u32>,
+    entrypoint: u32,
+}
+
+impl Program {
+    fn parse(program: &str) -> Result<Self, &'static str> {
+        let mut text = Vec::new();
+        let mut data = Vec::new();
+        let mut entrypoint = None;
+
+        for line in program.lines() {
+            if line.starts_with(";") {
+                continue; // skip comments
+            }
+
+            let (address, contents) = line.split_once(": ").ok_or("Failed to parse line!")?;
+            let index =
+                u32::from_str_radix(address, 16).or(Err("Failed to parse line address!"))?;
+            let (content, _comment) = contents.split_once(" ; ").unwrap_or((contents, ""));
+
+            if content.starts_with("<") && content.ends_with(">") {
+                if content == "<main>" {
+                    entrypoint.get_or_insert(index);
+                }
+                continue; // skip labels
+            }
+
+            let content = u32::from_str_radix(content.replace(" ", "").as_str(), 16)
+                .or(Err("Failed to parse line content!"))?;
+
+            if index < 0x0040_0000 {
+                return Err("Out-of-bounds memory assignment!");
+            } else if index <= 0x0FFF_FFFF {
+                let real_pc = ((index - 0x0040_0000) >> 2) as usize;
+                if real_pc != text.len() {
+                    return Err("Discontinuous .text block!");
+                }
+                text.push(content);
+            } else if index <= 0x7FFF_FFFF {
+                let real_in = ((index - 0x1001_0000) >> 2) as usize;
+                if real_in != data.len() {
+                    return Err("Discontinuous .data block!");
+                }
+                data.push(content);
+            }
+        }
+        Ok(Self {
+            text,
+            data,
+            stack: Vec::new(),
+            entrypoint: entrypoint.unwrap_or(0x0040_0000),
+        })
+    }
+
+    fn index(&mut self, index: u32) -> Result<&u32, i32> {
+        if index & 0b0011 != 0 {
+            eprintln!("Index must be word-aligned");
+            return Err(-1);
+        }
+
+        if index < 0x0040_0000 {
+            eprintln!("Out-of-bounds memory access.");
+            return Err(-1);
+        } else if index <= 0x0FFF_FFFF {
+            let real_pc = ((index - 0x0040_0000) >> 2) as usize;
+            if real_pc < self.text.len() {
+                return Ok(&self.text[real_pc]);
+            }
+        } else if index <= 0x7FFF_FFFF {
+            let real_in = ((index - 0x1001_0000) >> 2) as usize;
+            if real_in < self.data.len() {
+                return Ok(&self.data[real_in]);
+            }
+            let real_in = ((0x7FFF_FFFF - index) >> 2) as usize;
+            if real_in == self.stack.len() {
+                self.stack.push(0); // allocate more stack!
+            }
+            if real_in < self.stack.len() {
+                return Ok(&self.stack[real_in]);
+            }
+        }
+
+        eprintln!("Out-of-bounds memory access.");
+        Err(-1)
+    }
+
+    fn access(&mut self, index: u32) -> Result<&mut u32, i32> {
+        if index & 0b0011 != 0 {
+            eprintln!("Index must be word-aligned");
+            return Err(-1);
+        }
+
+        if index < 0x0040_0000 {
+            eprintln!("Out-of-bounds memory access.");
+            return Err(-1);
+        } else if index <= 0x0FFF_FFFF {
+            eprintln!("Tried to modify .text!");
+            return Err(-1);
+        } else if index <= 0x7FFF_FFFF {
+            let real_in = ((index - 0x1001_0000) >> 2) as usize;
+            if real_in < self.data.len() {
+                return Ok(&mut self.data[real_in]);
+            }
+            let real_in = ((0x7FFF_FFFF - index) >> 2) as usize;
+            if real_in == self.stack.len() {
+                self.stack.push(0); // allocate more stack!
+            }
+            if real_in < self.stack.len() {
+                return Ok(&mut self.stack[real_in]);
+            }
+        }
+
+        eprintln!("Out-of-bounds memory access.");
+        Err(-1)
+    }
+}
+
 struct Interpreter {
     pc: u32,
     reg_lo: u32,
     reg_hi: u32,
     registers: [u32; 32],
-    text: Vec<u32>,
+    program: Program,
 }
 
 impl Interpreter {
-    pub fn new() -> Self {
+    pub fn new(program: Program) -> Self {
         Interpreter {
-            pc: 0b0,
+            pc: program.entrypoint,
             reg_lo: 0,
             reg_hi: 0,
             registers: [0b0; 32],
-            text: Vec::new(),
+            program: program,
         }
-    }
-
-    fn load(&mut self, program: Vec<u32>) {
-        self.pc = 0b0;
-        self.text = program;
     }
 
     fn step(&mut self) -> Result<(), i32> {
-        if self.pc & 0b0011 != 0 {
-            eprintln!("Program Counter must be word-aligned");
-            return Err(-1)
-        }
-
-        let real_pc = (self.pc >> 2) as usize;
-
-        if real_pc >= self.text.len() {
-            eprintln!("Out-of-bounds memory access.");
-            return Err(-1)
-        }
-
-        let instr = self.text[real_pc];
+        let instr = *self.program.index(self.pc)?;
         self.pc += 0b100;
         self.execute(instr)
     }
 
-    fn syscall(&self) -> Result<(), i32> {
+    fn syscall(&mut self) -> Result<(), i32> {
         match self.registers[2] {
             1 => {
+                // Print Integer
                 print!("{}", self.registers[4] as i32);
                 Ok(())
             }
-            2 => {
-                print!("{}", f32::from_be_bytes(self.registers[4].to_be_bytes()));
-                Ok(())
-            }
             4 => {
-                // get the chars between self.registers[4] and self.registers[5]
-                print!("{}", "Hello World!\n");
+                // Print String
+                if self.registers[5] < self.registers[4] {
+                    return Err(-1);
+                }
+                let mut buf = String::new();
+                for i in self.registers[5]..self.registers[4] {
+                    let n = self.program.index(i).or(Err(-1))?;
+                    let c = char::from_u32(*n).ok_or(-1)?;
+                    buf.push(c);
+                }
+                print!("{}", buf);
                 Ok(())
             }
-            //   5 =>
-            //     {
-            //   static char str [256];
-
-            //   read_input (str, 256);
-            //   R[REG_RES] = atol (str);
-            //   break;
-            //     }
-
-            //   6 =>
-            //     {
-            //   static char str [256];
-
-            //   read_input (str, 256);
-            //   FPR_S (REG_FRES) = (float) atof (str);
-            //   break;
-            //     }
-
-            //   7 =>
-            //     {
-            //   static char str [256];
-
-            //   read_input (str, 256);
-            //   FPR [REG_FRES] = atof (str);
-            //   break;
-            //     }
-
-            //   case READ_STRING_SYSCALL:
-            //     {
-            //   read_input ( (char *) mem_reference (R[REG_A0]), R[REG_A1]);
-            //   data_modified = true;
-            //   break;
-            //     }
-
-            //   case SBRK_SYSCALL:
-            //     {
-            //   mem_addr x = data_top;
-            //   expand_data (R[REG_A0]);
-            //   R[REG_RES] = x;
-            //   data_modified = true;
-            //   break;
-            //     }
-
-            //   case PRINT_CHARACTER_SYSCALL:
-            //     write_output (console_out, "%c", R[REG_A0]);
-            //     break;
-
-            //   case READ_CHARACTER_SYSCALL:
-            //     {
-            //   static char str [2];
-
-            //   read_input (str, 2);
-            //   if (*str == '\0') *str = '\n';      /* makes xspim = spim */
-            //   R[REG_RES] = (long) str[0];
-            //   break;
-            //     }
-            16 => Err(0),
-            17 => Err(self.registers[4] as i32),
+            5 => {
+                // Read integer
+                let mut buf = String::new();
+                std::io::stdin().read_line(&mut buf).map_err(|_| -1)?;
+                let num = buf.parse::<i32>().map_err(|_| -1)?;
+                self.registers[2] = u32::from_ne_bytes(num.to_ne_bytes());
+                Ok(())
+            }
+            8 => {
+                // Read string
+                if self.registers[5] < self.registers[4] {
+                    return Err(-1);
+                }
+                let mut buf = String::new();
+                std::io::stdin().read_line(&mut buf).map_err(|_| -1)?;
+                let mut buf: std::slice::Iter<'_, [u8; 4]> =
+                    buf.as_bytes().as_chunks::<4>().0.into_iter();
+                for i in self.registers[5]..self.registers[4] {
+                    let n = self.program.access(i).or(Err(-1))?;
+                    if let Some(byte) = buf.next() {
+                        *n = u32::from_ne_bytes(*byte);
+                    } else {
+                        break;
+                    }
+                }
+                Ok(())
+            }
+            9 => Ok(()),  // Memory Allocation
+            10 => Err(0), // Exit
             _ => {
                 println!("Unrecognized Syscall");
                 Ok(())
@@ -208,6 +222,7 @@ impl Interpreter {
     }
 
     fn execute(&mut self, instr: u32) -> Result<(), i32> {
+        self.registers[0] = 0; // $zero is always 0 (easier than doing checks)
         let opcode = instr >> 26;
         match opcode {
             0x00 =>
@@ -239,7 +254,14 @@ impl Interpreter {
         }
     }
 
-    fn execute_rtype(&mut self, funct: u32, rs: usize, rt: usize, rd: usize, shift: u32) -> Result<(), i32> {
+    fn execute_rtype(
+        &mut self,
+        funct: u32,
+        rs: usize,
+        rt: usize,
+        rd: usize,
+        shift: u32,
+    ) -> Result<(), i32> {
         match funct {
             0x00 // sll
                 => self.registers[rd] = self.registers[rs] << shift,
@@ -323,7 +345,13 @@ impl Interpreter {
         Ok(())
     }
 
-    fn execute_itype(&mut self, opcode: u32, rs: usize, rd: usize, immediate: u16) -> Result<(), i32> {
+    fn execute_itype(
+        &mut self,
+        opcode: u32,
+        rs: usize,
+        rd: usize,
+        immediate: u16,
+    ) -> Result<(), i32> {
         let src = self.registers[rs];
         let dst = &mut self.registers[rd];
         match opcode {
@@ -351,10 +379,10 @@ impl Interpreter {
                 => *dst = src ^ immediate as u32,
             0x0f // lui
                 => *dst = ((immediate as u32) << 16) & 0xffff0000,
-            // 0x23 // lw
-            //    => 
-            // 0x2b // sw
-            //    => 
+            0x23 // lw
+                => *dst = *self.program.index(src + immediate as u32)?,
+            0x2b // sw
+               => *self.program.access(src + immediate as u32)? = *dst,
             _ => return self.segfault(),
         }
         Ok(())
@@ -365,5 +393,3 @@ fn jumpaddr(pc: u32, instr: u32) -> u32 {
     return (pc & 0b11110000_00000000_00000000_00000000)
         | ((instr & 0b00000011_11111111_11111111_11111111) << 2);
 }
-
-// https://www.yumpu.com/en/document/read/33742169/mips32-instruction-set-quick-reference-mips-technologies-inc
